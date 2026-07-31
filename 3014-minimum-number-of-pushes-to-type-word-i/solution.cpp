cf921af1d9cfbcec30714e0c72d09f09f0ef1c2d
divya gupta <divyagupta70600@gmail.com>
2026-07-31 16:16:31 +0530

    Solve Minimum Number of Pushes to Type Word I (cpp)

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+      int n=word.length();
+      int answer=0;
+      for(int i=0;i<n;i++){
+        answer += (i/8)+1;
+      }
+       return answer; 
+
+    }
+};

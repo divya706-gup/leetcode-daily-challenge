e6af57477942fa9e6e99016f3159a0b55783e1c7
divya gupta <divyagupta70600@gmail.com>
2026-07-31 16:16:52 +0530

    Solve Smallest Palindromic Rearrangement I (cpp)

---
+++ 3517-smallest-palindromic-rearrangement-i/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string smallestPalindrome(string s) {
+        int n=s.length();
+        string half = s.substr(0, n / 2);
+        sort(half.begin(),half.end());
+        string rev_half=half;
+        reverse(rev_half.begin(),rev_half.end());
+        if(n%2!=0){
+            return half +s[n/2]+rev_half;
+        }
+        return half + rev_half;
+    }
+};
